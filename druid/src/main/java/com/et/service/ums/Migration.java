b6d5cb9c1096e419b9053978221601d58b1a1a85
wxu <wxu@mail.etonenet.com>
2016-01-15 14:21:30 +0800

    线程迁移

--- druid/src/main/java/com/et/service/ums/Migration.java
+++ druid/src/main/java/com/et/service/ums/Migration.java
@@ -14,24 +14,53 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-public class Migration {
+public class Migration implements Runnable {
   public static final Logger log = LoggerFactory.getLogger(Migration.class);
 
   private static String insertSql =
-//      "INSERT INTO tt_mt (SP_MT_ID, SP_ID, SP_SERVICE_CODE, FROM_USER, TO_USER, TEMPLATE_ID, MSG_ID, MSG_STATUS, MSG_STATUS1, MSG_STATUS2, SP_MT_TIME, CARRIER_MT_TIME, CARRIER_RT_TIME, SP_RT_TIME, PROVINCE_CODE, CITY_CODE ) "
+      // "INSERT INTO tt_mt (SP_MT_ID, SP_ID, SP_SERVICE_CODE, FROM_USER, TO_USER, TEMPLATE_ID,
+      // MSG_ID, MSG_STATUS, MSG_STATUS1, MSG_STATUS2, SP_MT_TIME, CARRIER_MT_TIME, CARRIER_RT_TIME,
+      // SP_RT_TIME, PROVINCE_CODE, CITY_CODE ) "
       "INSERT INTO tt_mt VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )";
 
   private static Connection mysqlConnection;
   private static Connection mariaConnection;
 
-  public static void main(String[] args) throws Exception {
+  /** 间隔 */
+  private static final int STEP = 100;
+  /** 查询起始值 */
+  private static long start = 0 - STEP;
 
+  private static final int THREAD_MAX = 20;
+  private static int threadNum = 0;
+
+  public static void main(String arg[]) throws Exception {
     mysqlConnection = getMysqlConnection();
     mariaConnection = getMariaConnection();
-    execute();
+    newThread();
   }
 
-  private static void execute() throws SQLException, ParseException {
+  /** 开启进程 */
+  private synchronized static void newThread() {
+    if (threadNum < THREAD_MAX) {
+      threadNum++;
+      Thread thread = new Thread(new Migration());
+      thread.start();
+    }
+  }
+
+  private synchronized long threadStart() {
+    start += STEP;
+    return start;
+  }
+
+  private synchronized void threadEnd() {
+    threadNum--;
+  }
+
+  private void execute() throws SQLException, ParseException {
+    long i = threadStart();
+    log.info("thread start :{}", i);
     long start = System.currentTimeMillis();
     SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
     Date d1 = fmt.parse("2016-01-01");
@@ -41,19 +70,23 @@ public class Migration {
     PreparedStatement stmt = mysqlConnection.prepareStatement(sql);
     stmt.setLong(1, d1.getTime() * 10000);
     stmt.setLong(2, d2.getTime() * 10000);
-    stmt.setLong(3, 0);
-    stmt.setLong(4, 1000);
+    stmt.setLong(3, i);
+    stmt.setLong(4, STEP);
     ResultSet rs = stmt.executeQuery();
+    log.info("query cost time:{}", System.currentTimeMillis() - start);
 
-    log.info("query cost time:{}",System.currentTimeMillis()-start);
-    // Statement stmt = mysqlConnection.createStatement();
-    // ResultSet rs = stmt.executeQuery(sql);
+    if (rs.next()) {
+      // 开新线程
+      newThread();
+      // 插入到maria
+      insert(rs);
+    }
 
-    // 插入到maria
-    insert(rs);
 
     rs.close();
     stmt.close();
+
+    threadEnd();
   }
 
   /**
@@ -67,7 +100,7 @@ public class Migration {
    * @throws SQLException
    *
    */
-  private static void insert(ResultSet rs) throws SQLException {
+  private void insert(ResultSet rs) throws SQLException {
     long start = System.currentTimeMillis();
     ResultSetMetaData metaData = rs.getMetaData();
     int columnCount = metaData.getColumnCount();
@@ -81,11 +114,10 @@ public class Migration {
       }
       pstmt.addBatch();
     }
-    log.info("prepare cost time:{}",System.currentTimeMillis()-start);
     pstmt.executeBatch();
     mariaConnection.commit();
     pstmt.close();
-    log.info("insert cost time:{}",System.currentTimeMillis()-start);
+    log.info("insert cost time:{}", System.currentTimeMillis() - start);
   }
 
   private static Connection getMysqlConnection() {
@@ -117,5 +149,15 @@ public class Migration {
     }
 
     return connection;
+  }
+
+  @Override
+  public void run() {
+    try {
+      this.execute();
+    } catch (SQLException | ParseException e) {
+      e.printStackTrace();
+    }
+
   }
 }
